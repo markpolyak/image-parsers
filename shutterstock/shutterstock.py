8a3e7e4c93e74a8ad870c7a80562c458ecc7bb06
Mark Polyak <markpolyak@gmail.com>
2021-07-12 20:38:16 +0300

    Update shutterstock parser

--- shutterstock/shutterstock.py
+++ shutterstock/shutterstock.py
@@ -1,3 +1,4 @@
+import logging
 import urllib3
 import requests
 from bs4 import BeautifulSoup
@@ -5,26 +6,35 @@ import re
 import csv
 from PIL import Image
 import os
+import sys
 
 class ShutterstockParser:
 
     userAgent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) ..'}
+    labelsFile = 'labels.tsv'
     pageCount = 0
     loadedPageCount = 0
     imageFolderPath = ""
+    loadedImages = {}
 
     def __init__(self, imageFolderPath):
         self.imageFolderPath = imageFolderPath
 
     def parseAllPages(self):
-        loadedPageCount = self.__readLoadedPageCount()
-        pageCount = self.__getInitPageCount()
-        while pageCount > loadedPageCount:
-            self.parsePage(pageCount - loadedPageCount)
-            loadedPageCount += 1
-            self.__saveLoadedPageCount(loadedPageCount)
+        self.__loadImageIds(self.imageFolderPath)
+        self.loadedPageCount = self.__readLoadedPageCount()
+        logging.info("%d pages were already loaded", self.loadedPageCount)
+        self.pageCount = self.__getInitPageCount()
+        logging.info("Total %d pages are available for download", self.pageCount)
+        while self.pageCount > self.loadedPageCount:
+            self.parsePage(self.pageCount - self.loadedPageCount)
+            self.loadedPageCount += 1
+            self.__saveLoadedPageCount(self.loadedPageCount)
 
     def parsePage(self, pageId = 1):
+        logging.info("Parsing page %d out of %d ...", pageId, self.pageCount)
+        newImageCount = 0
+        dupImageCount = 0
         pageData = self.__loadPage(pageId)
         if pageData is not None:
             soup = BeautifulSoup(pageData, 'html.parser')
@@ -35,26 +45,39 @@ class ShutterstockParser:
                 if len(imageInfo):
                     content = str(imageInfo[0]).split('"')
 
-                    photoPath = content[19][3:len(content[19])]
+                    photoPath = content[19]#[3:len(content[19])]
                     imageId = photoPath.split("-")[len(photoPath.split("-")) - 1]
+                    # skip images that were already loaded
+                    if imageId in self.loadedImages:
+                        dupImageCount += 1
+                        continue
 
-                    discription = content[21].replace('\n', '').replace('\0', '').replace('\f', '').replace('\b', '').replace('\a', '').replace('\a', '"').strip()
-                    if re.search(r'[^a-zA-Z]', discription):
-                        imageInternetPath = "https://image.shutterstock.com" + photoPath + "-260nw-" + imageId + ".jpg"
-                        imageContent = self.__loadImage(imageInternetPath, imageId)
-                        if imageContent is not None:
-                            imageLocalPath = self.__saveLocalImage(imageId, imageContent)
-                            if imageLocalPath is not None:
-                                success = self.__cropImageCharacter(imageLocalPath)
-                                if success:
-                                    self.__saveImageInfo(imageLocalPath, discription, imageInternetPath)
-                                    newPageCount = self.__getPageCount(soup)
-                                    if newPageCount != 0:
-                                        pageCount = newPageCount
+                    description = content[21].replace('\n', '').replace('\0', '').replace('\f', '').replace('\b', '').replace('\a', '').replace('\a', '"').strip()
+                    # if re.search(r'[^a-zA-Z]', description):
+                        # try:
+                        #     imageInternetPath = imageInfo[0].img.get('src')
+                        # except Exception:
+                        #     logging.error("No image path found for image %s on page %d. Source: %s", imageId. pageId, imageInfo[0])
+                        #     continue
+                    imageInternetPath = "https://image.shutterstock.com" + photoPath + "-260nw-" + imageId + ".jpg"
+                    imageContent = self.__loadImage(imageInternetPath, imageId)
+                    if imageContent is not None:
+                        imageLocalPath = self.__saveLocalImage(imageId, imageContent)
+                        if imageLocalPath is not None:
+                            success = self.__cropImage(imageLocalPath)
+                            if success:
+                                self.__saveImageInfo(imageLocalPath, description, imageInternetPath)
+                                newImageCount += 1
+                                newPageCount = self.__getPageCount(soup)
+                                if newPageCount != 0:
+                                    self.pageCount = newPageCount
+            logging.info("Page had %d elements. %d images were saved. %d images were downloaded earlier.", len(imageTables), newImageCount, dupImageCount)
+        else:
+            logging.warning("Empty page %d", pageId)
 
     def __loadPage(self, pageId):
         try:
-            url = 'https://www.shutterstock.com/ru/search?image_type=photo&sort=newest&page=%d' % (pageId)
+            url = 'https://www.shutterstock.com/search?image_type=photo&sort=newest&page=%d' % (pageId)
 
             request = urllib3.PoolManager(10, self.userAgent)
             response = request.urlopen('GET', url)
@@ -66,6 +89,7 @@ class ShutterstockParser:
             return None
 
     def __loadImage(self, path, imageId):
+        logging.info("Downloading image %s from %s", imageId, path)
         try:
             response = requests.get(path, self.userAgent)
 
@@ -77,7 +101,7 @@ class ShutterstockParser:
 
     def __saveLocalImage(self, imageId, content):
         try:
-            path = self.imageFolderPath + imageId + ".jpg"
+            path = os.path.join(self.imageFolderPath, f"{imageId}.jpg")
             if os.path.isfile(path):
                 return None
             out = open(path, "wb")
@@ -87,10 +111,10 @@ class ShutterstockParser:
         except:
             return None
 
-    def __saveImageInfo(self, localImagePath, discription, internerImagePath):
-        with open('shutterstockImages.tsv', 'a') as out_file:
+    def __saveImageInfo(self, localImagePath, description, internetImagePath):
+        with open(self.labelsFile, 'a') as out_file:
             tsv_writer = csv.writer(out_file, delimiter='\t')
-            tsv_writer.writerow([localImagePath, discription, internerImagePath])
+            tsv_writer.writerow([localImagePath, description, internetImagePath])
 
     def __saveLoadedPageCount(self, cout):
         with open('loadedPageCount.tsv', 'wt') as out_file:
@@ -109,6 +133,7 @@ class ShutterstockParser:
 
     def __getInitPageCount(self):
         pageData = self.__loadPage(1)
+        # print(pageData)
         if pageData is not None:
             soup = BeautifulSoup(pageData, 'html.parser')
             return self.__getPageCount(soup)
@@ -116,12 +141,13 @@ class ShutterstockParser:
     def __getPageCount(self, soup):
         pageCountDiv = soup.find('div', {'class': 'b_aI_c6506'})
         pageCount = str(pageCountDiv).split(" ")[2][0:len(str(pageCountDiv).split(" ")[2]) - 6].replace(u'\xa0', u'')
+        pageCount = pageCount.replace(',', '')
 
         if pageCount.isdigit():
             return int(pageCount)
         return 0
 
-    def __cropImageCharacter(self, path):
+    def __cropImage(self, path):
         try:
             im = Image.open(path)
             img_width, img_height = im.size
@@ -129,9 +155,40 @@ class ShutterstockParser:
             im_crop.save(path, quality=95)
             return True
         except:
-            print("не удалось обрезать изображение", path)
+            logging.error("Unable to crop image %s", path)
             return False
 
-shutterstockParser = ShutterstockParser(imageFolderPath="/Users/kirill/Desktop/image/")
-shutterstockParser.parseAllPages()
+    def __loadImageIds(self, path):
+        logging.info("Loading image ids from %s", path)
+        imageFiles = {}
+        for entry in os.scandir(path):
+            if entry.is_file() and entry.name.endswith('.jpg'):
+                imageId, _ = os.path.splitext(entry.name)
+                imageFiles[imageId] = entry.name
+        with open(self.labelsFile) as f:
+            for line in f:
+                p = [x.strip() for x in line.rstrip('\n').split('\t')]
+                imageId, _ = os.path.splitext(p[0].split('/')[1])
+                if imageId in imageFiles:
+                    self.loadedImages[imageId] = imageFiles[imageId]
+                    del imageFiles[imageId]
+                else:
+                    logging.warning("Description without a source image file %s", p[0])
+        for imageId in imageFiles:
+            logger.warning("Image file without description %s", imageFiles[imageId])
+        logging.info("Total %d image ids loaded", len(self.loadedImages))
 
+
+def main():
+    logging.basicConfig(
+        handlers=[logging.StreamHandler(), logging.FileHandler(f"{sys.argv[0]}.log")],
+        level=logging.INFO,
+        format="%(asctime)s %(name)s - %(levelname)s - %(message)s"
+    )
+    shutterstockParser = ShutterstockParser(imageFolderPath="images/")
+    shutterstockParser.parseAllPages()
+
+
+
+if __name__ == '__main__':
+    main()
