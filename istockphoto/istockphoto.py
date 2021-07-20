38138ce986b22e3d71a713ed9d87b663731632b3
Mark Polyak <markpolyak@gmail.com>
2021-07-20 14:18:07 +0300

    Update ShutterStock and iStockPhoto parsers

--- istockphoto/istockphoto.py
+++ istockphoto/istockphoto.py
@@ -1,33 +1,54 @@
+import logging
 import requests
 import os
+import sys
 from bs4 import BeautifulSoup
 from time import sleep
 
-os.system('chcp 65001')
+
+logging_filename = f"{sys.argv[0]}_{sys.argv[1].replace(' ','_')}.log" if len(sys.argv) > 1 else f"{sys.argv[0]}.log"
+file_handler = logging.FileHandler(logging_filename)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+logging.basicConfig(
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(asctime)s %(name)s - %(levelname)s - %(message)s",
+    handlers=[console_handler, file_handler]
+)
+
+# os.system('chcp 65001')
 
 # Сайт к которому выполняется парсер
 site = 'https://www.istockphoto.com'
 
 # Поисковый запрос
-search = ''
+# search = ''
+search = sys.argv[1] if len(sys.argv) > 1 else ''
 # Параметры авторизации
-username = ''
-password = ''
+username = 'markpolyak@smedx.com'
+password = 'asdf1234'
 # Путь к папке с изображениями
-download = ''
+download = sys.argv[2] if len(sys.argv) > 2 else 'images'
 # Путь к файлу номера последней обработанной страницы
-path_page = download + ''
+path_page = 'loaded_pages.txt'
 # Путь к файлу описаний к изображениям
-path_tsv = download + ''
+path_tsv = 'labels.tsv'
+
+logging.info("Search query: %s. Download folder: %s. Captions file: %s. Loaded pages counter: %s", search, download, path_tsv, path_page)
 
 # Адресная строка для страницы авторизации
 url = site + '/sign-in'
 # Создание сессии и авторизация
 session = requests.session()
-loging = session.post(url, {
+response = session.post(url, {
     'new_session[username]': username,
     'new_session[password]': password
 })
+
+if response.status_code != 200:
+    logging.critical("Unable to initialize session. Response code %d. Response headers %s. Response text %s", response.status_code, response.headers, response.text)
+    sys.exit(1)
 
 
 # Присваивание номера первой страницы поискового запроса
@@ -41,7 +62,9 @@ else:
 
 # Адресная строка для поискового запроса
 url = site + '/en/search/2/image/'
-parameters = {'phrase': search, 'page': last_page, 'mediatype': 'photography'}
+parameters = {'phrase': search, 'page': last_page, 'mediatype': 'photography', 'sort': 'newest'}
+if len(search) < 1:
+    del parameters['phrase']
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/75.0.3770.142 Safari/537.36'
@@ -60,9 +83,13 @@ is_not_first = False
 # Присвоение номера последенй страницы поискового запроса
 last_page_num = soup.find('span', class_='PaginationRow-module__lastPage___1gtme').text
 last_page_num = int(last_page_num)
+logging.info("Total %d pages are available for download", last_page_num)
+
+repeat_on_error_counter = 0
 
 # Цикл по обработке страниц
 while parameters['page'] <= last_page_num:
+    logging.info("Parsing page %d out of %d ...", parameters['page'], last_page_num)
     # Обработка первой итерации
     if is_not_first:
         response = session.get(url, params=parameters)
@@ -71,37 +98,68 @@ while parameters['page'] <= last_page_num:
         is_not_first = True
     # Проверка на ошибки в сессии
     if response.status_code == 500:
+        logging.error("Response code %d for url %s with params %s", response.status_code, url, parameters)
         continue
     if response.status_code == 404:
+        logging.critical("Response code %d for url %s with params %s", response.status_code, url, parameters)
         break
+    if response.status_code == 301:
+        logging.warning("Response code %d for url %s with params %s", response.status_code, url, parameters)
     # Открытие файла для выгрузки описаний
     tsv_file = open(path_tsv, 'a', encoding='utf-8')
     # Цикл для парсинга страницы
-    for parse in soup.find_all('div', class_='FixedAsset-module__galleryFixedAsset___2QSXC'):
+    image_elements = soup.find_all('div', class_='FixedAsset-module__galleryFixedAsset___2QSXC')
+    if len(image_elements) < 1:
+        logging.error("Unable to parse page %d. No images were found", parameters['page'])
+        # Ждём, пока блокировка не отключится
+        sleep(time_to_sleep_when_captcha)
+        time_to_sleep_when_captcha += 1
+        repeat_on_error_counter += 1
+        if repeat_on_error_counter > 100:
+            logging.critical("Too many page download attempts have failed")
+            sys.exit(1)
+        else:
+            continue
+    else:
+        repeat_on_error_counter = 0
+    new_image_count = 0
+    dup_image_count = 0
+    err_count = 0
+    for element in image_elements:
+        image_id = -1
         # Поимка ошибки по капче
         try:
             # Получение id изображения
-            img_id = parse['data-asset-id']
+            img_id = element['data-asset-id']
             path = download + '/' + img_id + '.jpg'
             # Проверка на дубликаты
             if not os.path.exists(path):
                 # Получение описания
-                title = parse.find('div', class_="FixedAssetContent-module__title___2W6Re", title=True)
+                title = element.find('div', class_="FixedAssetContent-module__title___2W6Re", title=True)
+                if not title:
+                    logging.error("Title not found for image %s on page %d. Page contents: %s", img_id, parameters['page'], element)
                 title = title['title'].replace('\n', ' ')
                 # Получение изображения
-                img = parse.find('img', src=True)
+                img = element.find('img', src=True)
                 src = img['src']
                 img_data = session.get(src).content
                 # Выгрузка изображения и описания к нему
                 img_file = open(path, 'wb+')
                 img_file.write(img_data)
-                tsv_file.write(download + img_id + '.jpg' + '\t' + title + '\t' + src + '\n')
+                tsv_file.write(path + '\t' + title + '\t' + src + '\n')
                 img_file.close()
-        except:
+                new_image_count += 1
+            else:
+                dup_image_count += 1
+                logging.warning("File %s already exists", path)
+        except Exception:
+            logging.exception("Unable to parse page %d, download image %s or save it to disk", parameters['page'], image_id)
             # Ждём, пока капча не отпустит
             sleep(time_to_sleep_when_captcha)
             time_to_sleep_when_captcha += 1
-            print('error')
+            err_count += 1
+            # print('error')
+    logging.info("Page had %d elements. %d images were saved. %d images were downloaded earlier. %d images caused a parser error.", len(image_elements), new_image_count, dup_image_count, err_count)
     time_to_sleep_when_captcha = 5
     tsv_file.close()
     # Запоминаем номер обработанной страницы
--- shutterstock/shutterstock.py
+++ shutterstock/shutterstock.py
@@ -7,30 +7,50 @@ import csv
 from PIL import Image
 import os
 import sys
+import uuid
 
 class ShutterstockParser:
 
     userAgent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) ..'}
-    labelsFile = 'labels.tsv'
+    labelsFile = "labels.tsv"
+    offsetFile = "loadedPageCount.tsv"
+    otherDataSources = "data.txt"
     pageCount = 0
     loadedPageCount = 0
     imageFolderPath = ""
+    startFromLastPage = True
     loadedImages = {}
     session = None
 
-    def __init__(self, imageFolderPath):
+    def __init__(self, imageFolderPath, labelsFile=None, offsetFile=None, startFromLastPage=True):
         self.imageFolderPath = imageFolderPath
+        if labelsFile:
+            self.labelsFile = labelsFile
+        if offsetFile:
+            self.offsetFile = offsetFile
+        self.startFromLastPage = startFromLastPage
         self.session = requests.Session()
         self.session.headers.update(self.userAgent)
 
     def parseAllPages(self):
-        self.__loadImageIds(self.imageFolderPath)
+        self.__loadImageIds(self.imageFolderPath, self.labelsFile)
+        if self.otherDataSources:
+            with open(self.otherDataSources) as f:
+                for line in f:
+                    p = [x.strip() for x in line.strip().split(',')]
+                    if len(p) > 1:
+                        self.__loadImageIds(p[0], p[1])
+                    else:
+                        logger.warning("Unable to parse line %s from %s", line, self.otherDataSources)
         self.loadedPageCount = self.__readLoadedPageCount()
         logging.info("%d pages were already loaded", self.loadedPageCount)
         self.pageCount = self.__getInitPageCount()
         logging.info("Total %d pages are available for download", self.pageCount)
         while self.pageCount > self.loadedPageCount:
-            self.parsePage(self.pageCount - self.loadedPageCount)
+            if self.startFromLastPage:
+                self.parsePage(self.pageCount - self.loadedPageCount)
+            else:
+                self.parsePage(self.loadedPageCount)
             self.loadedPageCount += 1
             self.__saveLoadedPageCount(self.loadedPageCount)
 
@@ -55,7 +75,7 @@ class ShutterstockParser:
                         dupImageCount += 1
                         continue
 
-                    description = content[21].replace('\n', '').replace('\0', '').replace('\f', '').replace('\b', '').replace('\a', '').replace('\a', '"').strip()
+                    description = content[21].replace('\n', ' ').replace('\t', ' ').replace('\0', ' ').replace('\f', ' ').replace('\b', ' ').replace('\a', ' ').replace('\r', ' ').strip()
                     # if re.search(r'[^a-zA-Z]', description):
                         # try:
                         #     imageInternetPath = imageInfo[0].img.get('src')
@@ -70,6 +90,7 @@ class ShutterstockParser:
                             success = self.__cropImage(imageLocalPath)
                             if success:
                                 self.__saveImageInfo(imageLocalPath, description, imageInternetPath)
+                                self.loadedImages[imageId] = imageLocalPath
                                 newImageCount += 1
                                 newPageCount = self.__getPageCount(soup)
                                 if newPageCount != 0:
@@ -91,31 +112,36 @@ class ShutterstockParser:
                 return response.content
             logging.error("Page %d not loaded. Response code %s, content length %d", pageId, response.status_code, len(response.content))
             return None
-        except Exception as e:
+        except Exception:
             logging.exception("Exception while loading page %d", pageId)
             return None
 
     def __loadImage(self, path, imageId):
         logging.info("Downloading image %s from %s", imageId, path)
         try:
-            response = requests.get(path, self.userAgent)
+            # response = requests.get(path, self.userAgent)
+            response = self.session.get(path)
 
             if response.content and response.status_code == 200:
                 return response.content
+            logging.error("Failed to download image %s. Response code %s, content length %d", imageId, response.status_code, len(response.content))
             return None
-        except:
+        except Exception:
+            logging.exception("Exception while loading image %s", imageId)
             return None
 
     def __saveLocalImage(self, imageId, content):
         try:
             path = os.path.join(self.imageFolderPath, f"{imageId}.jpg")
             if os.path.isfile(path):
+                logging.warning("Can't save image %s as it already exists", imageId)
                 return None
             out = open(path, "wb")
             out.write(content)
             out.close()
             return path
-        except:
+        except Exception:
+            logging.exception("Unable to save image %s", imageId)
             return None
 
     def __saveImageInfo(self, localImagePath, description, internetImagePath):
@@ -124,13 +150,13 @@ class ShutterstockParser:
             tsv_writer.writerow([localImagePath, description, internetImagePath])
 
     def __saveLoadedPageCount(self, cout):
-        with open('loadedPageCount.tsv', 'wt') as out_file:
+        with open(self.offsetFile, 'wt') as out_file:
             tsv_writer = csv.writer(out_file, delimiter='\t')
             tsv_writer.writerow([cout])
 
     def __readLoadedPageCount(self):
         try:
-            tsv_file = open("loadedPageCount.tsv")
+            tsv_file = open(self.offsetFile)
             read_tsv = csv.reader(tsv_file, delimiter="\t")
             cout = int(list(read_tsv)[0][0])
             tsv_file.close()
@@ -165,18 +191,18 @@ class ShutterstockParser:
             logging.error("Unable to crop image %s", path)
             return False
 
-    def __loadImageIds(self, path):
+    def __loadImageIds(self, path, labelsFile):
         logging.info("Loading image ids from %s", path)
         imageFiles = {}
         for entry in os.scandir(path):
             if entry.is_file() and entry.name.endswith('.jpg'):
                 imageId, _ = os.path.splitext(entry.name)
                 imageFiles[imageId] = entry.name
-                if len(imageFiles) % 100000 == 0:
+                if (len(imageFiles) % 100000) == 0:
                     logging.info("%d image files processed", len(imageFiles))
         logging.info("Total of %d image files were found", len(imageFiles))
         logging.info("Loading descriptions from %s", self.labelsFile)
-        with open(self.labelsFile) as f:
+        with open(labelsFile) as f:
             for line in f:
                 p = [x.strip() for x in line.rstrip('\n').split('\t')]
                 if len(p) < 1:
@@ -194,12 +220,25 @@ class ShutterstockParser:
 
 
 def main():
+    logging_filename = f"{sys.argv[0]}_{sys.argv[1]}.log" if len(sys.argv) > 1 else f"{sys.argv[0]}.log"
     logging.basicConfig(
-        handlers=[logging.StreamHandler(), logging.FileHandler(f"{sys.argv[0]}.log")],
+        handlers=[logging.StreamHandler(), logging.FileHandler(logging_filename)],
         level=logging.INFO,
         format="%(asctime)s %(name)s - %(levelname)s - %(message)s"
     )
-    shutterstockParser = ShutterstockParser(imageFolderPath="images/")
+    labelsFile = None
+    offsetFile = None
+    imageFolderPath = "images/"
+    startFromLastPage = True
+    if len(sys.argv) > 1:
+        imageFolderPath = sys.argv[1]
+    if len(sys.argv) > 2:
+        labelsFile = sys.argv[2]
+    if len(sys.argv) > 3:
+        offsetFile = sys.argv[3]
+    if len(sys.argv) > 4:
+        startFromLastPage = (int(sys.argv[4])==1)
+    shutterstockParser = ShutterstockParser(imageFolderPath=imageFolderPath, labelsFile=labelsFile, offsetFile=offsetFile, startFromLastPage=startFromLastPage)
     shutterstockParser.parseAllPages()
 
 
